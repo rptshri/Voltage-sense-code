5d441e7218d53604b3c018ff92dbe7a02f9fb5dc
-rptshri_09 <rptshrivastava@gmail.com>
2018-12-22 00:40:43 +0530

    added functionalities

--- voltage_sense_code.ino
+++ voltage_sense_code.ino
@@ -4,8 +4,10 @@ int sensorPin = A0;    // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
 int maxValue = 0;
 int minValue = 0;
-int const arr_size = 192;
-int sample[arr_size];
+int avgValue = 0;
+int acVoltage = 0;
+int const arr_size = 192; //9600 is default sampling rate
+int sample[arr_size]; //array for storing values of every sample
 
 void setup() {
   // declare the ledPin as an OUTPUT:
@@ -14,26 +16,29 @@ void setup() {
 }
 
 void loop() {
-  // read the value from the sensor:
- int i;
-  for (i = 0; i < arr_size; i = i + 1) {
-  sensorValue = analogRead(sensorPin);
-
-  float value = (float)sensorValue;
-  value /= 1023.0;
-  value *= 5000; // convert value in mv between 0 to 5000
-  sensorValue = (int)value;
-  //  Serial.println(sensorValue);
-
- 
+  // read the value from the sensor and store it in an array:
+  int i;
+  for (i = 0; i < arr_size; i = i + 1)
+  {
+    sensorValue = analogRead(sensorPin);
+    float value = (float)sensorValue;
+    value /= 1023.0;
+    value *= 5000; // convert value in mv between 0 to 5000
+    sensorValue = (int)value;
+    //  Serial.println(sensorValue);
     sample[i] = sensorValue;
   }
-  Serial.println(max_value_in_arr(sample, arr_size));
-  
-}
+  Serial.println(max_value_in_arr(sample, arr_size)); //print Vmax in milli volts
+  Serial.println(min_value_in_arr(sample, arr_size)); //print Vmin in milli volts
+  avgValue = (maxValue + minValue) / 2;
+  Serial.println(avgValue);
 
+acVoltage = ((maxValue-2.5)/0.0909)*18.06932 ;
+
+}
+///////////////////////////////////////////////////////////function for maximum value in array
 int max_value_in_arr(int sample[], int size)
-{ 
+{
   maxValue = sample[0];
   int i = 0;
   for (i = 0; i < arr_size; i++)
@@ -44,4 +49,18 @@ int max_value_in_arr(int sample[], int size)
     }
   }
   return maxValue;
+}
+///////////////////////////////////////////////////////////function for minimum value in array
+int min_value_in_arr(int sample[], int size)
+{
+  minValue = sample[0];
+  int i = 0;
+  for (i = 0; i < arr_size; i++)
+  {
+    if (sample[i] < maxValue)
+    {
+      minValue = sample[i];
+    }
+  }
+  return minValue;
 }
